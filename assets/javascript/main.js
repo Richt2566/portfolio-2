3481b379939ad5c6af887477afb06dee482ddbeb
Rich T <rtranter25@gmail.com>
2018-04-03 18:46:40 -0500

    trying to get the development section to work

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,3 +11,19 @@ $('a[href^="#"]').on('click', function(event) {
 
 });
 
+$("#projectchi").on("click", function(){
+	$(".chicago-desc").fadeToggle(1000);
+})
+
+$("#projectgame").on("click", function(){
+	$(".game-desc").fadeToggle(1000);
+})
+
+$("#projectondeck").on("click", function(){
+	$(".ondeck-desc").fadeToggle(1000);
+})
+
+$("#projectdeath").on("click", function(){
+	$(".death-desc").fadeToggle(1000);
+})
+
